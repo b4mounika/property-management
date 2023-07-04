b806aed61b7118110b1f68d7fae9602a2666e440
Srinivas1925usa <32499930+Srinivas1925usa@users.noreply.github.com>
2023-07-04 09:40:45 -0700

    implemented user registration functionality

--- src/main/java/com/example/demo/controller/CalculatorMain.java
+++ src/main/java/com/example/demo/controller/CalculatorMain.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CalculatorMain {
 public static void main(String[] args) {
     CalculatorController cc = new CalculatorController();
-    Double result = cc.add(6.5,4.5,5.7);
+ Double  result = cc.add(6.5,4.5,5.7);
+
+    log.info("the result is {}",String.valueOf(result));
+
 }
 
 }
--- src/test/java/com/example/demo/Demo4ApplicationTests.java
+++ src/test/java/com/example/demo/Demo4ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Demo4ApplicationTests {
 
     @Test
     void contextLoads() {
+        String expected = "Baeldung";
+        String actual = "Baeldung";
+
+        assertEquals(expected, actual);
     }
 
 }
